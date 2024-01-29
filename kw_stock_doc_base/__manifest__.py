77d2fc3686880ec82dc5ecee377aac029e761d42
ERPBOXUA <115533947+ERPBOXUA@users.noreply.github.com>
2024-01-29 11:14:25 +0200

    Add files via upload

--- kw_stock_doc_base/__manifest__.py
+++ kw_stock_doc_base/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Stock print document for Ukraine',
 
-    'version': '16.0.1.0.2',
+    'version': '14.0.1.0.2',
     'author': 'Kitworks Systems',
     'website': 'https://kitworks.systems/',
     'license': 'OPL-1',
--- kw_stock_ttn/__manifest__.py
+++ kw_stock_ttn/__manifest__.py
@@ -1,21 +1,19 @@
 {
     'name': 'Tovarno Transportna Nakladna for Ukraine',
-    'summary': 'Tovarno Transportna Nakladna Товарно-транспортна накладна Бланк "Товарно транспортна накладна" '
-               'друк/ документ/ Товарно транспортна накладна/ТТН/Товаро-транспортна',
+    'summary': 'Tovarno Transportna Nakladna Товарно-транспортна накладна',
 
     'author': 'Kitworks Systems',
     'website': 'https://kitworks.systems/',
 
     'category': 'Accounting',
     'license': 'LGPL-3',
-    'version': '16.0.1.0.5',
+    'version': '14.0.1.0.2',
 
-    'depends': ['sale', 'stock', 'kw_stock_doc_base'],
+    'depends': ['stock', 'kw_stock_doc_base'],
 
     'data': [
         'report/report.xml',
         'report/templates.xml',
-        'views/stock_picking_views.xml',
     ],
 
     'installable': True,
