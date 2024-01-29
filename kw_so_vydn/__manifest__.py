eece4d420821a752d67f243907a9aaacda85f498
ERPBOXUA <115533947+ERPBOXUA@users.noreply.github.com>
2024-01-29 11:13:04 +0200

    Add files via upload

---
+++ kw_so_vydn/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Vydatkova Nakladna SO for Ukraine',
+    'summary': 'Vydatkova Nakladna Видаткова накладна',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Accounting',
+    'license': 'LGPL-3',
+    'version': '14.0.1.0.3',
+
+    'depends': ['sale', 'kw_so_doc_base', 'kw_discount_sum'],
+
+    'data': [
+        'report/report.xml',
+        'report/templates.xml',
+    ],
+
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+
+}
