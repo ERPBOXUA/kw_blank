d693fc059647f67f7683d5057e72b4c223c4a863
ERPBOXUA <115533947+ERPBOXUA@users.noreply.github.com>
2024-01-29 11:08:57 +0200

    Add files via upload

---
+++ kw_so_chek/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Tovarniy Chek SO for Ukraine',
+    'summary': 'Tovarniy Chek Товарний чек',
+
+    'author': 'Kitworks Systems',
+    'website': 'https://kitworks.systems/',
+
+    'category': 'Accounting',
+    'license': 'LGPL-3',
+    'version': '14.0.1.0.3',
+
+    'depends': ['sale', 'kw_so_doc_base', 'kw_discount_sum'],
+
+    'data': [
+        'report/report.xml',
+        'report/templates.xml',
+    ],
+
+    'installable': True,
+
+    'images': [
+        'static/description/cover.png',
+        'static/description/icon.png',
+    ],
+
+}
